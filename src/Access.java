c7fa62eda659fe8e4e06e4dcd9da074d6a2cff00
Bikash <bikash_shs@yahoo.com>
2013-09-21 00:35:07 -0500

    initial commit

---
+++ src/Access.java
@@ -0,0 +1,19 @@
+
+public class Access {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Subscriber s=new Subscriber();
+		Subscriber s1=new Subscriber("Ram", 543);
+		System.out.println("sub name: "+s.name);
+		System.out.println("sub n0=  "+s.no);
+		System.out.println("sub1 name: "+s1.name);
+		System.out.println("sub1 n0=  "+s1.no);
+		s.makecall();
+		s1.makecall();
+		s.Subscriber();
+	}
+
+}
---
+++ src/Subscriber.java
@@ -0,0 +1,20 @@
+
+public class Subscriber {
+
+String name;
+int no;
+public Subscriber(){
+	name="Bikash";
+	no=456;
+	}
+public Subscriber(String name, int sno){
+	this.name=name;
+	no=sno;
+}
+public void Subscriber(){
+	System.out.println(" made");
+}
+public void makecall(){
+	System.out.println("call made");
+	}
+}
